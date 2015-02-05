2ad3fa5477f6733da3706b348d11b14350516ef3
yogaswara <gistha.yogaswara@gmail.com>
2015-02-05 16:15:15 +0700

    update js

--- partials/defaultjs.blade.php
+++ partials/defaultjs.blade.php
@@ -1,10 +1,10 @@
 	<!-- Default js -->
+	{{HTML::script('themes/'.$toko->akunId.'-tema/shion/assets/js/jquery-1.7.2.min.js')}}
 	{{HTML::script('themes/'.$toko->akunId.'-tema/shion/assets/js/bootstrap.min.js')}}
-	{{HTML::script('themes/'.$toko->akunId.'-tema/shion/assets/js/bootstrap-dropdown.min.js')}}
+	{{HTML::script('themes/'.$toko->akunId.'-tema/shion/assets/js/bootstrap-dropdown.js')}}
 	{{HTML::script('themes/'.$toko->akunId.'-tema/shion/assets/js/jquery.fancybox.js')}}
 	{{HTML::script('themes/'.$toko->akunId.'-tema/shion/assets/js/jquery.flexslider.js')}}
 	{{HTML::script('themes/'.$toko->akunId.'-tema/shion/assets/js/jquery.sharrre.js')}}
-	{{HTML::script('themes/'.$toko->akunId.'-tema/shion/assets/js/jquery-1.7.2.min.js')}}
 	{{HTML::script('themes/'.$toko->akunId.'-tema/shion/assets/js/page.js')}}
 	{{HTML::script('themes/'.$toko->akunId.'-tema/shion/assets/js/respond.js')}}
 	{{HTML::script('themes/'.$toko->akunId.'-tema/shion/assets/js/sharrre.js')}}
